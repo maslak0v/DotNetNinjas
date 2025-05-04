dfb434bc94a79c35aa38a79d1c089ad05d0f4681
Daria Pritchina <dasha.bayanova@gmail.com>
2025-05-04 13:36:56 +0400

    T14 add expenses controller template

---
+++ FinancialTracker.Services.AnalyticsAPI/Controllers/ExpensesApiController.cs
@@ -0,0 +1,34 @@
+using FinancialTracker.Services.AnalyticsAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialTracker.Services.AnalyticsAPI.Controllers;
+
+[ApiController]
+[Route("api/expenses")]
+public class ExpensesApiController : ControllerBase
+{
+    private readonly ResponseDto _response;
+
+    public ExpensesApiController()
+    {
+        _response = new ResponseDto();
+    }
+    
+    [HttpGet]
+    public IActionResult Get(
+        [FromQuery] Guid userId,
+        [FromQuery] DateTime startDate,
+        [FromQuery] DateTime endDate)
+    {
+        try
+        {
+            _response.Result = "Hello";
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return Ok(_response);
+    }
+}
---
+++ FinancialTracker.Services.AnalyticsAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,8 @@
+namespace FinancialTracker.Services.AnalyticsAPI.Models.Dto;
+
+public class ResponseDto
+{
+    public object? Result { get; set; }
+    public bool IsSuccess { get; set; } = true;
+    public string Message { get; set; } = "";
+}
