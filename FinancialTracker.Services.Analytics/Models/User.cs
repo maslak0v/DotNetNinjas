44fb3617bd839ff02406bf916e5905b9a0d3bf10
Daria Pritchina <dasha.bayanova@gmail.com>
2025-05-06 16:06:58 +0400

    T14 Add Guid to User table

--- FinancialTracker.Services.Analytics/Models/User.cs
+++ FinancialTracker.Services.Analytics/Models/User.cs
@@ -1,10 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinancialTracker.Services.Analytics.Models;
 
+[Index(nameof(Guid), IsUnique = true)]
 public class User
 {
     [Key]
     public int UserId { get; set; }
+    [Required]
+    public Guid Guid { get; set; }
     public string Name { get; set; } = string.Empty;
 }
