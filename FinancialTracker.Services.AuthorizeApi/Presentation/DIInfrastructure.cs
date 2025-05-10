8fd79197a03c40742c59a0280b3543c8f97ee112
Dovmial <nma91@mail.ru>
2025-05-10 21:00:54 +0500

    Init authApi service

---
+++ FinancialTracker.Services.AuthorizeApi/Presentation/DIInfrastructure.cs
@@ -0,0 +1,18 @@
+﻿namespace FinancialTracker.Services.AuthorizeApi.Presentation
+{
+    public static class DIInfrastructure
+    {
+        public static IServiceCollection AddPresentationLayer(
+                this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            //services.AddScoped<ICurrentUserService, CurrentUserService>();
+            return services;
+        }
+    }
+
+}
