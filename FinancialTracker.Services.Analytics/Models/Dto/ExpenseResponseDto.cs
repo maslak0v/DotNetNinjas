c8c28ad89e8bcb43fc5a60506f25682a64fcea38
Daria Pritchina <dasha.bayanova@gmail.com>
2025-05-14 13:36:18 +0400

    T14 make Expense.User property required, change type for Amount to decimal. Add migrations. Add NoTracking flag.

--- FinancialTracker.Services.Analytics/Models/Dto/ExpenseResponseDto.cs
+++ FinancialTracker.Services.Analytics/Models/Dto/ExpenseResponseDto.cs
@@ -3,6 +3,6 @@ namespace FinancialTracker.Services.Analytics.Models.Dto;
 public class ExpenseResponseDto
 {
     public DateTime ExpenseTime { get; set; }
-    public double Amount { get; set; }
+    public decimal Amount { get; set; }
     public string Currency { get; set; } = "RUB";
 }
--- FinancialTracker.Services.Analytics/Models/Expense.cs
+++ FinancialTracker.Services.Analytics/Models/Expense.cs
@@ -8,11 +8,11 @@ public class Expense
     [Key]
     public int ExpenseId { get; set; }
     [ForeignKey("UserId")]
-    public User? User { get; set; }
+    public required User User { get; set; }
     [Required]
     public DateTime ExpenseTime { get; set; }
     [Required]
-    public double Amount { get; set; }
+    public decimal Amount { get; set; }
     
     // Новые поля для счета и валюты
     public Guid AccountId { get; set; }
