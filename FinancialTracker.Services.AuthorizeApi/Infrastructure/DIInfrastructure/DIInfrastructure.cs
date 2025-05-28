302a75ddee99151f180ee1e7f3762b8ea569b353
Dovmial <nma91@mail.ru>
2025-05-28 12:58:59 +0500

    DIInfrasructure extension is divided into methods

--- FinancialTracker.Services.AuthorizeApi/Infrastructure/DIInfrastructure/DIInfrastructure.cs
+++ FinancialTracker.Services.AuthorizeApi/Infrastructure/DIInfrastructure/DIInfrastructure.cs
@@ -19,34 +19,28 @@ namespace FinancialTracker.Services.AuthorizeApi.Infrastructure.DIInfrastructure
             this IServiceCollection services,
             IConfiguration configuration)
         {
+            string? jwtkey = GetSecretKey();
+            JwtSettings? jwtSettings = JwtSettingsRegistration(services, configuration);
 
-            var jwtSettings = configuration
-                .GetSection(nameof(JwtSettings))
-                .Get<JwtSettings>();
-            var jwtkey = Environment.GetEnvironmentVariable("JWT_KEY");
-            if (string.IsNullOrWhiteSpace(jwtkey))
-                throw new Exception("Jwtkey is not configured");
-            //JwtSettings registration 
-            //(To inject by IOptions<JwtSettings>)
-            services.Configure<JwtSettings>(configuration.GetSection(nameof(JwtSettings)));
+            DbContextRegistration(services);
+            SetDbContextToIdentity(services);
 
-            string? connectionString = Environment.GetEnvironmentVariable("AUTH_DB_CONNECTION_STRING");
-            if (string.IsNullOrWhiteSpace(connectionString))
-                throw new Exception("Connection string for db is empty");
+            Registration_AuthenticationJwt(services, jwtkey, jwtSettings);
 
-            //DbContext registration
-            services.AddDbContext<AuthDbContext>(options
-                => options.UseNpgsql(connectionString));
+            AddScopedServices(services);
 
-            //Set dbContext for identity
-            services.AddIdentity<AuthUser, IdentityRole>(options =>
-            {
-                options.Password.RequiredLength = 8;
-                options.Password.RequireDigit = false;
-                options.User.RequireUniqueEmail = true;
-                options.SignIn.RequireConfirmedEmail = false;
-            }).AddEntityFrameworkStores<AuthDbContext>();
+            return services;
+        }
 
+
+        #region private
+        private static void AddScopedServices(IServiceCollection services)
+        {
+            services.AddScoped<ITokenService<AuthUser>, TokenServiceImpl>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+        private static void Registration_AuthenticationJwt(IServiceCollection services, string jwtkey, JwtSettings? jwtSettings)
+        {
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -55,21 +49,65 @@ namespace FinancialTracker.Services.AuthorizeApi.Infrastructure.DIInfrastructure
             {
                 options.TokenValidationParameters = new TokenValidationParameters()
                 {
-                    ValidateIssuer = false,
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.ValidIssuer,
                     ValidateAudience = false,
+                    ValidAudience = jwtSettings.ValidAudience,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtSettings.ValidIssuer,
-                    ValidAudience = jwtSettings.ValidAudience,
                     IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes(jwtkey))
+                        Encoding.UTF8.GetBytes(jwtkey)),
+                    ClockSkew = TimeSpan.Zero
                 };
             });
+        }
 
-            services.AddScoped<ITokenService<AuthUser>, TokenServiceImpl>();
-            services.AddScoped<IUserRepository, UserRepository>();
+        /// <summary>
+        /// Get key from some store
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private static string GetSecretKey()
+        {
+            var jwtkey = Environment.GetEnvironmentVariable("JWT_KEY");
+            if (string.IsNullOrWhiteSpace(jwtkey))
+                throw new Exception("Jwtkey is not configured");
+            return jwtkey;
+        }
 
-            return services;
+        private static void SetDbContextToIdentity(IServiceCollection services)
+        {
+            services.AddIdentity<AuthUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = false;
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = false;
+            }).AddEntityFrameworkStores<AuthDbContext>()
+              .AddDefaultTokenProviders();
+        }
+
+        private static void DbContextRegistration(IServiceCollection services)
+        {
+            //check connection string
+            string? connectionString = Environment.GetEnvironmentVariable("AUTH_DB_CONNECTION_STRING");
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new Exception("Connection string for db is empty");
+
+            //DbContext registration
+            services.AddDbContext<AuthDbContext>(options
+                => options.UseNpgsql(connectionString));
+        }
+
+        private static JwtSettings? JwtSettingsRegistration(IServiceCollection services, IConfiguration configuration)
+        {
+            //JwtSettings registration 
+            //(To inject by IOptions<JwtSettings>)
+            var sectionJwt = configuration
+                .GetSection(nameof(JwtSettings));
+            services.Configure<JwtSettings>(sectionJwt);
+            var jwtSettings = sectionJwt.Get<JwtSettings>();
+            return jwtSettings;
         }
 
         public static async Task ApplyMigrationsAsync(this IApplicationBuilder app)
@@ -79,5 +117,6 @@ namespace FinancialTracker.Services.AuthorizeApi.Infrastructure.DIInfrastructure
                 scope.ServiceProvider.GetRequiredService<AuthDbContext>();
             await dbContext.Database.MigrateAsync();
         }
+        #endregion
     }
 }
