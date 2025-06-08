aad370ba5f52e22b5125d66f1c7d446cffade516
ulyanovav <ulyanovav@intellect-soft.ru>
2025-06-08 15:31:34 +0400

    Добавлен запуск миграции при старте проекта

--- Wallet.API/Program.cs
+++ Wallet.API/Program.cs
@@ -1,28 +1,45 @@
-namespace Wallet.API;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using Wallet.Infrastructure.Data;
 
-public class Program
+namespace Wallet.API
 {
-    private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
-        .ConfigureAppConfiguration(
-            (hostingContext, builder) =>
-            {
-                builder
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.Development.json", false, true)
-                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true,
-                        true)
-                    .AddEnvironmentVariables();
-            })
-        .ConfigureWebHostDefaults(
-            webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-                webBuilder.ConfigureKestrel(
-                    options => { options.AddServerHeader = false; });
-            });
-
-    public static void Main(string[] args)
+    public class Program
     {
-        CreateHostBuilder(args).Build().Run();
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(
+                (hostingContext, builder) =>
+                {
+                    builder
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.Development.json", false, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddEnvironmentVariables();
+                })
+            .ConfigureWebHostDefaults(
+                webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(
+                        options => { options.AddServerHeader = false; });
+                });
+
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            // Применяем миграции при запуске
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<WalletPostgresDbContext>();
+                context.Database.Migrate();
+            }
+            
+            host.Run();
+        }
     }
 }
