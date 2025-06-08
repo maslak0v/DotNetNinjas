e2fd8e1c13ee8695313a441a625324d3f71d65f6
ulyanovav <ulyanovav@intellect-soft.ru>
2025-06-08 15:32:27 +0400

    Удалены лишние using из program

--- Wallet.API/Program.cs
+++ Wallet.API/Program.cs
@@ -1,8 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.IO;
 using Wallet.Infrastructure.Data;
 
 namespace Wallet.API
