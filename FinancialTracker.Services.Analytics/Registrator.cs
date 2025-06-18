320274c6caaf7dc669988e0c85df65a348e94267
Daria Pritchina <dasha.bayanova@gmail.com>
2025-06-18 17:45:16 +0400

    T18 change connection string for docker env, apply migration when app is starting

--- FinancialTracker.Services.Analytics/Registrator.cs
+++ FinancialTracker.Services.Analytics/Registrator.cs
@@ -1,8 +1,10 @@
 using AutoMapper;
+using FinancialTracker.Services.Analytics.DataAccess;
 using FinancialTracker.Services.Analytics.DataAccess.Repositories;
 using FinancialTracker.Services.Analytics.Mapping;
 using FinancialTracker.Services.Analytics.Services;
 using FinancialTracker.Services.Analytics.Services.Implementation;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinancialTracker.Services.Analytics;
 
@@ -22,6 +24,15 @@ public static class Registrator
             .AddScoped<IExpensesRepository, ExpensesRepository>();
 
         return serviceCollection;
+    }
+    
+    public static async Task<IApplicationBuilder> ApplyMigrationsAsync(this IApplicationBuilder app)
+    {
+        await using var scope = app.ApplicationServices.CreateAsyncScope();
+        await using var dbContext =
+            scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await dbContext.Database.MigrateAsync();
+        return app;
     }
     
     public static IServiceCollection InstallAutoMapper(this IServiceCollection services)
