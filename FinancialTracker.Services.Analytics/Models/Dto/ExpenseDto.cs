41e7e353bf0cb66435009d17d131f14028f2c11a
Daria Pritchina <dasha.bayanova@gmail.com>
2025-07-14 16:00:54 +0400

    T17 add restrictions to ExpenseDto

--- FinancialTracker.Services.Analytics/Models/Dto/ExpenseDto.cs
+++ FinancialTracker.Services.Analytics/Models/Dto/ExpenseDto.cs
@@ -2,10 +2,10 @@ namespace FinancialTracker.Services.Analytics.Models.Dto;
 
 public class ExpenseDto
 {
-    public Guid ExpenseId { get; set; }
-    public Guid UserId { get; set; }
-    public Guid AccountId { get; set; }
-    public string? Category { get; set; }
-    public DateTime ExpenseTime { get; set; }
-    public decimal Amount { get; set; }
+    public required Guid ExpenseId { get; init; }
+    public required Guid UserId { get; init; }
+    public required Guid AccountId { get; init; }
+    public string? Category { get; init; }
+    public required DateTime ExpenseTime { get; init; }
+    public required decimal Amount { get; init; }
 }
