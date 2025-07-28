683d345ddda50fa0d256a79804062bd5dca3f8c8
Dovmial <nma91@mail.ru>
2025-07-28 18:25:07 +0500

    fix: Operationresult FromException -> public

--- Wallet.Application/Helpers/OperationResult.cs
+++ Wallet.Application/Helpers/OperationResult.cs
@@ -20,7 +20,7 @@ namespace Wallet.Application.Helpers
 
         public static OperationResult Failure(Enum_StatusCode statusCode, string message)
             => new OperationResult(false, statusCode, message);
-        private static OperationResult FromException(Exception ex)
+        public static OperationResult FromException(Exception ex)
         {
             StringBuilder sb = new StringBuilder();
             for (var current = ex; current is not null; current = current.InnerException)
