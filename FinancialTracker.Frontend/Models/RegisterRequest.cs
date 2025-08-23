3a80523a4804821a816d6e801377ba9958d776b0
Mikhail Nekliudov <nma91@mail.ru>
2025-08-23 13:24:21 +0500

    fix: fullname with numbers (blazor)

--- FinancialTracker.Frontend/Models/RegisterRequest.cs
+++ FinancialTracker.Frontend/Models/RegisterRequest.cs
@@ -5,7 +5,7 @@ namespace FinancialTracker.Frontend.Models;
 public class RegisterRequest
 {
     [Required(ErrorMessage = "Обязательное поле")]
-    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Имя должно содержать только символы латинского алфавита")]
+    [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Имя может содержать только символы латинского алфавита и цифры")]
     [MaxLength(50, ErrorMessage = "Не более 50 символов")]
     public string FullName { get; set; }
 
