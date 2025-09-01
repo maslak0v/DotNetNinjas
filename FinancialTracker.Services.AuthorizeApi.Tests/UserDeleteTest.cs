03891a984cebebf8aac172fa7372b9c9f993025d
Mikhail Nekliudov <nma91@mail.ru>
2025-09-01 14:20:28 +0500

    test: for  user delete usecase

---
+++ FinancialTracker.Services.AuthorizeApi.Tests/UserDeleteTest.cs
@@ -0,0 +1,47 @@
+﻿using FinancialTracker.Services.AuthorizeApi.Application.Fabrics;
+using FinancialTracker.Services.AuthorizeApi.Application.Features;
+using FinancialTracker.Services.AuthorizeApi.Application.Interfaces;
+using FinancialTracker.Services.AuthorizeApi.Application.UseCases.Interfaces;
+using FinancialTracker.Services.AuthorizeApi.Domain.ValueObjects;
+using FinancialTracker.Services.AuthorizeApi.Tests.Helpers;
+using Moq;
+namespace FinancialTracker.Services.AuthorizeApi.Tests
+{
+    public class UserDeleteTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public async Task UserDelete(int userId)
+        {
+            //Arrange
+
+
+            var mockResultDeleting = userId == 1
+                ? OperationResultCreator.Success(Enum_StatusCode.NO_CONTENT, string.Empty)
+                : OperationResultCreator.Failure(Enum_StatusCode.NOT_FOUND, "NotFound");
+                
+            var mockUserRepository = new Mock<IUserRepository>();
+            mockUserRepository
+                .Setup(repo => repo.DeleteAsync(It.IsAny<string>(), CancellationToken.None))
+                .ReturnsAsync(mockResultDeleting);
+
+            IAuthUseCaseFabric fabric = AuthUseCaseFabricCreator.Create();
+            IAuthUseCasesFacade facade = AuthUseCaseFacadeCreator.Create(fabric);
+
+            //Act
+            var result = await facade.DeleteAsync(Guid.CreateVersion7().ToString(), mockUserRepository.Object, CancellationToken.None);
+
+            //Assert
+            if (userId == 1)
+                Assert.Equal(Enum_StatusCode.NO_CONTENT, result.StatusCode);
+            else
+            {
+                Assert.False(result.IsSuccess);
+                Assert.False( string.IsNullOrEmpty(result.Message));
+                Assert.Equal(Enum_StatusCode.NOT_FOUND, result.StatusCode);
+            }
+
+        }
+    }
+}
