24519870115980c5a9eddc1576608d3c584f88b5
Mikhail Nekliudov <nma91@mail.ru>
2025-09-01 15:49:34 +0500

    feat: user delete messaging

---
+++ FinancialTracker.Services.Analytics/Messaging/UserDeletedAnalyticsConsumer.cs
@@ -0,0 +1,18 @@
+﻿using FinancialTracker.Services.Analytics.DataAccess.Repositories;
+using MassTransit;
+using MessageBus.Shared.Contracts.Interfaces;
+
+namespace FinancialTracker.Services.Analytics.Messaging
+{
+    public class UserDeletedAnalyticsConsumer(
+        ILogger<UserDeletedAnalyticsConsumer> logger,
+        IUserRepository userRepository) : IConsumer<IUserDeleted>
+    {
+        public async Task Consume(ConsumeContext<IUserDeleted> context)
+        {
+            var msg = context.Message;
+            logger.LogInformation("[RabbitMQ] User deleted: {userId}", msg.UserId);
+            await userRepository.DeleteByIdAsync(msg.UserId, context.CancellationToken);
+        }
+    }
+}
