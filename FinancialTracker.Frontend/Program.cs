f1ff2d5f2cc700f26e14cbca27a8934a78d7f248
Mikhail Nekliudov <nma91@mail.ru>
2025-09-24 20:08:38 +0500

    feat: add mudBlazo and dashboard sceleton

--- FinancialTracker.Frontend/Program.cs
+++ FinancialTracker.Frontend/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FinancialTracker.Frontend;
 using FinancialTracker.Frontend.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +13,8 @@ builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("http://l
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<BrowserStorage>();
 
-builder.Services.AddSingleton(builder.Configuration);
+//builder.Services.AddSingleton(builder.Configuration);
+builder.Services.AddMudServices();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+await app.RunAsync();
