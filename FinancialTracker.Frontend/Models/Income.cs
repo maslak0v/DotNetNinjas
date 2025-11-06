7ec689ca7195c5447c5bb9209f2464a3cbe9b754
DBKai <dbkai@ya.ru>
2025-11-06 18:32:55 +0500

    fix: changed income model props

--- FinancialTracker.Frontend/Models/Income.cs
+++ FinancialTracker.Frontend/Models/Income.cs
@@ -4,17 +4,13 @@ namespace FinancialTracker.Frontend.Models
 {
 	public class Income
 	{
-		[JsonPropertyName("incomeId")]
-		public required Guid IncomeId { get; set; }
-		[JsonPropertyName("userId")]
-		public required Guid UserId { get; set; }
-		[JsonPropertyName("accountId")]
-		public required Guid AccountId { get; set; }
 		[JsonPropertyName("category")]
 		public string? Category { get; set; }
-		[JsonPropertyName("incomeTime")]
-		public required DateTime IncomeTime { get; set; }
 		[JsonPropertyName("amount")]
 		public required decimal Amount { get; set; }
+		[JsonPropertyName("currency")]
+		public required string Currency { get; set; }
+		[JsonPropertyName("incomeTime")]
+		public required DateTime IncomeTime { get; set; }
 	}
 }
