d5d7bb1a740fa25b05dcc288777ef73a17bed1c9
DBKai <dbkai@ya.ru>
2025-11-06 13:47:36 +0500

    fix: changed type for balance

--- FinancialTracker.Frontend/Models/Account.cs
+++ FinancialTracker.Frontend/Models/Account.cs
@@ -5,7 +5,7 @@
 		public Guid AccountId { get; set; }
 		public Guid UserId { get; set; }
 		public string Name { get; set; }
-		public double CurrentBalance { get; set; }
+		public decimal CurrentBalance { get; set; }
 		public int Currency { get; set; }
 		public string CreatedAt { get; set; }
 		public string? UpdatedAt { get; set; }
